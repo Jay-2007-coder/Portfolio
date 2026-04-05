47a6701e2e85d3b0d4c0f583b60201eff23f0b8d
Jay <jaytalekar82@gmail.com>
2026-04-05 21:15:57 +0530

    fix(build): correct gsap types and ts undefined errors

--- src/types/gsap.d.ts
+++ src/types/gsap.d.ts
@@ -1,1 +1,7 @@
-declare module 'gsap-trial/SplitText'
+declare module 'gsap-trial/SplitText' {
+  export const SplitText: any;
+}
+
+declare module 'gsap-trial/ScrollSmoother' {
+  export const ScrollSmoother: any;
+}
